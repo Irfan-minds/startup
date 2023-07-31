4d02c1688afca4ffb8abaa967cc053fa62f5bb87
Irfan-minds <141023960+Irfan-minds@users.noreply.github.com>
2023-07-31 15:39:08 +0530

    Add files via upload

---
+++ Swimlane.js
@@ -0,0 +1,11 @@
+const InProgressLane = {
+  name: 'In Progress',
+  color: '#00ff00',
+};
+
+const CompleteLane = {
+  name: 'Complete',
+  color: '#0000ff',
+};
+
+const laneStatuses = [InProgressLane, CompleteLane];
